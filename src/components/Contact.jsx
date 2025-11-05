a3ffe884d96b6f3d9d52d29d9246f85c6c9ce299
dmennenoh <dmennenoh95@gmail.com>
2025-11-05 09:59:07 -0600

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,6 +9,7 @@ function Contact()
             <ul className="contact">
                 <li><span className='blueText'>Email: </span>dmennenoh95@gmail.com</li>
                 <li><span className='blueText'>LinkedIn: </span><a href='http://www.linkedin.com/in/dmennenoh' target='_blank' rel='noopener noreferrer'>linkedin.com/in/dmennenoh</a></li>
+                 <li><span className='blueText'>ArtStation: </span><a href='http://www.artstation.com/dmennenoh' target='_blank' rel='noopener noreferrer'>artstation.com/dmennenoh</a></li>
                 <li>I live in SE Wisconsin with my beautiful wife Jacqueline (<a href='http://www.jacqlines.com' target='_blank' rel='noopener noreferrer'>jacqlines.com</a>) and my two boys Kaden (11) and Kian (7)</li>
             </ul>            
             <img width='90%' height='auto' src='/assets/thefam.jpg' alt='the family'/>
